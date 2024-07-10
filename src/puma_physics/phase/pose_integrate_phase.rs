58d53a95dfcf5004cdf76a019b032dace701161e
AsagiPuma <AsagiPuma@126.com>
2024-07-10 15:18:48 +0800

    implement a gravity calculation

--- src/puma_physics/phase/pose_integrate_phase.rs
+++ src/puma_physics/phase/pose_integrate_phase.rs
@@ -1,11 +1,14 @@
-use bevy::{prelude::Query, transform::components::Transform};
+use bevy::{prelude::{Query, Res}, transform::components::Transform};
 
-use crate::entity::Rigidbody;
+use crate::{entity::Rigidbody, simulation::PumaPhysicsSettings};
 
 pub fn pose_integrate_phase(
-    mut query: Query<(&mut Transform, &mut Rigidbody)>
+    physics_settings: Res<PumaPhysicsSettings>,
+    mut query: Query<&mut Rigidbody>
 ) {
-    for(mut transform, mut rigidbody) in &mut query.iter_mut(){
-        // TODO
+    for mut rigidbody in &mut query.iter_mut(){
+        let mut new_velocity = rigidbody.linear_velocity();
+        new_velocity += physics_settings.delta_time * physics_settings.gravity_acceleration;
+        rigidbody.set_linear_velocity(new_velocity);
     }
 }
--- src/puma_physics/simulation.rs
+++ src/puma_physics/simulation.rs
@@ -4,8 +4,18 @@ use crate::phase::pose_integrate_phase;
 
 pub struct PumaPhysicsPlugin;
 
+#[derive(Resource)]
+pub struct PumaPhysicsSettings {
+    pub delta_time : f32,
+    pub gravity_acceleration : Vec3
+}
+
 impl Plugin for PumaPhysicsPlugin {
     fn build(&self,app: &mut App) {
-        app.add_systems(Update, pose_integrate_phase);
+        app.insert_resource(PumaPhysicsSettings {
+            delta_time: 0.02,
+            gravity_acceleration : Vec3::new(0.0, -9.8, 0.0)
+        });
+        app.add_systems(FixedUpdate, pose_integrate_phase);
     }
 }
